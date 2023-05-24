19725714fa90545f51d99e204d5b80b49b8c01eb
Negin <negin.khademi@gmail.com>
2023-05-25 02:15:05 +0430

    get data from server and dynamic data

--- script.js
+++ script.js
@@ -1,6 +1,94 @@
+const initialFacts = [
+  {
+    id: 1,
+    text: 'React is being developed by Meta (formerly facebook)',
+    source: 'https://opensource.fb.com/',
+    category: 'technology',
+    votesInteresting: 24,
+    votesMindblowing: 9,
+    votesFalse: 4,
+    createdIn: 2021,
+  },
+  {
+    id: 2,
+    text: 'Millennial dads spend 3 times as much time with their kids than their fathers spent with them. In 1982, 43% of fathers had never changed a diaper. Today, that number is down to 3%',
+    source: 'https://www.mother.ly/parenting/millennial-dads-spend-more-time-with-their-kids',
+    category: 'society',
+    votesInteresting: 11,
+    votesMindblowing: 2,
+    votesFalse: 0,
+    createdIn: 2019,
+  },
+  {
+    id: 3,
+    text: 'Lisbon is the capital of Portugal',
+    source: 'https://en.wikipedia.org/wiki/Lisbon',
+    category: 'society',
+    votesInteresting: 8,
+    votesMindblowing: 3,
+    votesFalse: 1,
+    createdIn: 2015,
+  },
+];
+
+const CATEGORIES = [
+  { name: 'technology', color: '#3b82f6' },
+  { name: 'science', color: '#16a34a' },
+  { name: 'finance', color: '#ef4444' },
+  { name: 'society', color: '#eab308' },
+  { name: 'entertainment', color: '#db2777' },
+  { name: 'health', color: '#14b8a6' },
+  { name: 'history', color: '#f97316' },
+  { name: 'news', color: '#8b5cf6' },
+];
+
+// Selecting DOM elements
 const btn = document.querySelector('.btn-open');
 const form = document.querySelector('.fact-form');
+const factList = document.querySelector('.fact-list');
 
+// Create DOM elements: Render facts in list
+factList.innerHTML = '';
+
+loadFacts();
+// Load data from supabase
+async function loadFacts() {
+  const res = await fetch('https://yjipgkjmbouxvuwrwzsu.supabase.co/rest/v1/facts', {
+    headers: {
+      apikey:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlqaXBna2ptYm91eHZ1d3J3enN1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODQ2NzgxNTEsImV4cCI6MjAwMDI1NDE1MX0.K0_apSWGNH26BdHXqlFmJoO4klddDjUd9EHvVJcS0Pw',
+    },
+    authorization:
+      'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlqaXBna2ptYm91eHZ1d3J3enN1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODQ2NzgxNTEsImV4cCI6MjAwMDI1NDE1MX0.K0_apSWGNH26BdHXqlFmJoO4klddDjUd9EHvVJcS0Pw',
+  });
+  const data = await res.json();
+  // console.log(data);
+  // const filteredData = data.filter((fact) => fact.category === 'society');
+  createFactsList(data);
+}
+// createFactsList(initialFacts);
+function createFactsList(dataArray) {
+  // factList.insertAdjacentElement("afterbegin",'<li>Mike</li>')
+
+  const htmlArr = dataArray.map(
+    (fact) => `
+    <li class="fact">
+      <p>
+        ${fact.text}
+        <a href="${fact.source}" rel="noopener" class="source" target="_blank">(source)</a>
+        </p>
+        <span class="tag" style="background-color: ${CATEGORIES.find((cat) => cat.name === fact.category).color}">${
+      fact.category
+    }</span>
+    </li>`
+  );
+  // console.log(htmlArr);
+  const html = htmlArr.join('');
+  // console.log(html);
+  factList.insertAdjacentHTML('afterbegin', html);
+}
+
+// Toggle form visibility
 btn.addEventListener('click', function () {
   if (form.classList.contains('hidden')) {
     form.classList.remove('hidden');
